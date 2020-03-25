63c78d68e678c852d49e4ebecc51cb856c9057a8
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-03-25 15:48:10 +0900

    comit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_042513) do
+ActiveRecord::Schema.define(version: 2020_03_23_045207) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -42,25 +42,25 @@ ActiveRecord::Schema.define(version: 2020_03_24_042513) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "nickname", null: false
-    t.string "profile"
-    t.string "assessment"
+    t.string "first_furigana"
+    t.string "last_furigana"
+    t.integer "birthday", null: false
+    t.string "image"
     t.integer "phone_number", null: false
     t.string "gender", null: false
-    t.string "birthday", null: false
-    t.string "image"
+    t.string "assessment"
+    t.string "profile"
     t.string "favorite"
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_furigana"
-    t.string "last_furigana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
