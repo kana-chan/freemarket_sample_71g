d6cd7c5bc0f779b405f1b77970d5cea926b1461a
kana-chan <bkm0115_y@icloud.com>
2020-03-25 21:52:32 +0900

    change view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-   @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,13 +14,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-   @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-   @item = Item.find(params[:id])
-   Item.update(item_params)
-   redirect_to item_path(item_id)
+    @item = Item.find(params[:id])
+    Item.update(item_params)
+    redirect_to item_path(item_id)
   end
 
   def destroy
