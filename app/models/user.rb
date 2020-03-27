87f30295c02c7fcf7e70d740bdd05261739e4893
kana-chan <bkm0115_y@icloud.com>
2020-03-27 10:00:38 +0900

    delete validation at models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
       
-         validates :nickname, :email, presence: true, uniqueness: true
-         validates :first_name, :last_name, :password, :phone_number, :gender, :image, presence: true
-         validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_furigana, :last_furigana, format: { with: /\A[ぁ-んー－]+\z/ }
-         validates :phone_number, format: { with: /\A\d{10,11}\z/ }
          has_one :address
 end
