1a5a1f2ab2e68fc0f6aa6228e6de2cb457da8708
kana-chan <bkm0115_y@icloud.com>
2020-03-27 10:05:26 +0900

    delete validate at migration file

--- db/migrate/20200319071510_devise_create_users.rb
+++ db/migrate/20200319071510_devise_create_users.rb
@@ -1,24 +1,24 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :nickname, null: false, unique: true
-      t.string :email,              null: false, default: ""
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :nickname
+      t.string :email, default: ""
+      t.string :first_name
+      t.string :last_name
       t.string :first_furigana
       t.string :last_furigana
-      t.string :birthday, null: false
+      t.string :birthday
       t.string :image
-      t.string :phone_number, null: false
-      t.string :gender, null: false
+      t.string :phone_number
+      t.string :gender
       
       t.string :assessment
       t.string :profile
       t.string :favorite
-      t.integer :prefecture_id, null:false
+      t.integer :prefecture_id
       ## Database authenticatable
       
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -46,11 +46,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps 
     end
 
-    add_index :users, :email, unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email
+    add_index :users, :reset_password_token
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/migrate/20200324013946_create_addresses.rb
+++ db/migrate/20200324013946_create_addresses.rb
@@ -1,14 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.string :last_name, null:false
-      t.string :first_name, null:false
-      t.string :last_furigana, null:false
-      t.string :first_furigana, null:false
-      t.string :zip, null:false
-      t.integer :prefecture_id, null:false
-      t.string :city, null:false, null:false
-      t.string :street, null:false
+      t.string :last_name
+      t.string :first_name
+      t.string :last_furigana
+      t.string :first_furigana
+      t.string :zip
+      t.integer :prefecture_id
+      t.string :city
+      t.string :street
       t.string :building_name
       t.string :phone_number
       t.references :user, foreign_key: true
