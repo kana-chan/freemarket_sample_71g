fe0a4a79dca66e294d49e7d7661076e3a43ad124
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-03-31 20:33:09 +0900

    OK

---
+++ db/migrate/20200319050631_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name, null:false
+      t.string :explaination, null:false
+      t.integer :condition_id, null:false 
+      t.integer :shipment_id, null:false
+      t.integer :responsibility_id, null:false
+      t.integer :price, null:false
+      t.integer :prefecture_id, null:false
+      t.string :brand
+      t.references :category, foreign_key: {to_tabel: :categories}
+      t.timestamps
+    end 
+  end
+end
+
+
