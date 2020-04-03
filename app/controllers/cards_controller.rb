03c9b66dd2f34b0c5932289de0f68a4fa10d83cb
umerina <61220531+umerina@users.noreply.github.com>
2020-04-03 16:00:49 +0900

    firstcommit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -17,14 +17,13 @@ class CardsController < ApplicationController
 
   # 登録画面で入力した情報をDBに保存
   def create
-    Payjp.api_key = "sk_test_a61f106182797a9972516b52"
+    Payjp.api_key = "sk_test_6d6a0e9384940def850311dd"
     if params['payjp-token'].blank?
       redirect_to root_path
     else
       customer = Payjp::Customer.create( # ここで先ほど生成したトークンを顧客情報と紐付け、PAY.JP管理サイトに送信
         email: current_user.email,
         card: params['payjp-token'],
-        metadata: {user_id: current_user.id} # 記述しなくても大丈夫です
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  require 'payjp'
+
+  def pay
+    Payjp.api_key = "sk_test_6d6a0e9384940def850311dd"
+    Payjp::Charge.create(
+      amount: 1100, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると生成されるトークン
+      currency: 'jpy'
+    )
+    redirect_to root_path
+  end
+  
+  
   def show
   end
 
