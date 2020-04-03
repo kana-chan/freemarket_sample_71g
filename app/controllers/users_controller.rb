7b2284d211c3cbfc49f6ea47ef5dde89d5b1cf0f
umerina <61220531+umerina@users.noreply.github.com>
2020-04-03 16:24:04 +0900

    price

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,10 @@ class UsersController < ApplicationController
 
   def pay
     Payjp.api_key = "sk_test_6d6a0e9384940def850311dd"
+    #@creditcard = Creditcard.where(user_id: current_user.id).first
+    @item = Item.find(params[:id])
     Payjp::Charge.create(
-      amount: 1100, # 決済する値段
+      amount: @item.price,
       card: params['payjp-token'], # フォームを送信すると生成されるトークン
       currency: 'jpy'
     )
