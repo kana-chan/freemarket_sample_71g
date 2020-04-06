79f42f57d462cf144df559fb412175165c567e5d
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-06 16:17:56 +0900

    js-error-ok

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -11,10 +11,10 @@ $(function(){
                   </div>`;
     return html;
   }
-  //const buildImg = (index, url)=> {
-    //const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-    //return html;
-  //}
+  // const buildImg = (index, url)=> {
+  //   const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+  //   return html;
+  // }
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5];
@@ -26,20 +26,20 @@ $(function(){
 
   $('#image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
-    ファイルのブラウザ上でのURLを取得する
+    // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-  }
     // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      fileIndexの先頭の数字を使ってinputを作る
+      // $('#previews').append(buildImg(targetIndex, blobUrl));
+      // fileIndexの先頭の数字を使ってinputを作る
     $('#image-box').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
@@ -49,7 +49,7 @@ $(function(){
     // もしチェックボックスが存在すればチェックを入れる
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().remove();
-    //$(`img[data-index="${targetIndex}"]`).remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
