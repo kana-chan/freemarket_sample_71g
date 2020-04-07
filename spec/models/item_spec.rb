e51d6224f7583cf35f9723787265a9ad659052aa
Yoshi-Kyoto5 <mxpx7simple7@gmail.com>
2020-04-07 14:13:09 +0900

    add test code of edit

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,6 +5,7 @@ describe Item do
     it "is valid with a name, explaination, condition_id, responsibility_id, 
         prefecture_id, shipment_id, category_id, user_id, price" do
       # item = build(:item)
+      # item = build(:item_with_image)
       item = create(:item_with_image)
     end
   end
