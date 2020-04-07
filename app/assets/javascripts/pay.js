fb17c62bd8127c6ea0272e2f358cd5164d08e74d
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-04-07 16:58:43 +0900

    完了

--- app/assets/javascripts/pay.js
+++ app/assets/javascripts/pay.js
@@ -4,7 +4,7 @@ $(function(){
 
   submit.addEventListener('click', function(e){  // 追加するボタンが押されたらイベント発火
     e.preventDefault();  // ボタンを一旦無効化
-    Payjp.setPublicKey("pk_test_17407eb0553356dd94e4eb41");
+    Payjp.setPublicKey("pk_test_e417bc230b8470066f4249e1");
     var card = {  // 入力されたカード情報を取得
       number: document.getElementById("card_number").value,
       exp_month: document.getElementById("exp_month").value,
