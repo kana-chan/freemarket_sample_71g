e67295931a1d47cf32cc53db4bfec263c2686db8
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-04-07 15:09:51 +0900

    完了

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -30,6 +30,7 @@ class CardsController < ApplicationController
  
 
   def destroy #PayjpとCardデータベースを削除します
+    card = Card.where(user_id: current_user.id).first
     unless card.blank?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
@@ -42,7 +43,7 @@ class CardsController < ApplicationController
   
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    
+    card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to action: "new" 
     else
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :set_item, only: [:show, :edit, :update, :destroy,:done]
+before_action :set_item, only: [:show, :edit, :update, :destroy, :done, :purchase]
 
   def set_item
     @item = Item.find(params[:id])
@@ -81,13 +81,13 @@ before_action :set_item, only: [:show, :edit, :update, :destroy,:done]
   end
 
   def done
-    
+   
   end
    
 
   def purchase
     card = Card.where(user_id: current_user.id).first
-    @item = Item.find(params[:id])
+    
     if card.blank?
       
       redirect_to controller: "cards", action: "new"
